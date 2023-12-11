610a4a4303066a473ea19797318d5f9d11916fa9
Elias Moura <elias.smourayt@gmail.com>
2023-12-11 13:54:12 -0300

    Utilizando Templete Method para reduzir a repetição de código nas classes filhas.

--- src/alura/descontoChainOfResponsability/Desconto.java
+++ src/alura/descontoChainOfResponsability/Desconto.java
@@ -5,9 +5,16 @@ import alura.orcamento.Orcamento;
 import java.math.BigDecimal;
 
 public abstract class Desconto {
-    protected Desconto proximo;
-    public abstract BigDecimal calcular(Orcamento orcamento);
     public Desconto(Desconto proximo) {
         this.proximo = proximo;
+    }
+    protected Desconto proximo;
+    protected abstract BigDecimal efetuarCalculo(Orcamento orcamento);
+    public abstract boolean deveAplicar(Orcamento orcamento);
+    public BigDecimal calcular(Orcamento orcamento){
+        if (deveAplicar(orcamento)){
+            return efetuarCalculo(orcamento);
+        }
+        return proximo.calcular(orcamento);
     }
 }
