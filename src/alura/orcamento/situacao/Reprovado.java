976129404f5dde4173be4ef7bd311b1f59c3d823
Elias Moura <elias.smourayt@gmail.com>
2023-12-12 13:45:30 -0300

    Classe para o estado Reprovado.

---
+++ src/alura/orcamento/situacao/Reprovado.java
@@ -0,0 +1,10 @@
+package alura.orcamento.situacao;
+
+import alura.orcamento.Orcamento;
+
+public class Reprovado extends SituacaoOrcamento {
+    @Override
+    public void finalizar(Orcamento orcamento) {
+        orcamento.setSituacao(new Finalizado());
+    }
+}
