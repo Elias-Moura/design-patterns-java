9bbf8102a6147228fb77affc043ae1b03317859d
Elias Moura <elias.smourayt@gmail.com>
2023-12-12 13:43:11 -0300

    Orçamento implementando o pattern state para aprovar, reprovar e finalizar.

---
+++ src/alura/orcamento/Orcamento.java
@@ -0,0 +1,43 @@
+package alura.orcamento;
+
+import alura.orcamento.situacao.EmAnalise;
+import alura.orcamento.situacao.SituacaoOrcamento;
+
+import java.math.BigDecimal;
+
+public class Orcamento {
+    private BigDecimal valor;
+    private SituacaoOrcamento situacao;
+    private int qunatidadeItens;
+
+    public Orcamento(BigDecimal valor, int qunatidadeItens) {
+        this.valor = valor;
+        this.qunatidadeItens = qunatidadeItens;
+        this.situacao = new EmAnalise();
+    }
+    public BigDecimal getValor() {
+        return valor;
+    }
+    public void aplicarDecontoExtra(){
+        BigDecimal valorDoDescontoExtra = this.situacao.aplicarDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+    public void aprovar(){
+        this.situacao.aprovar(this);
+    }
+    public void reporvar(){
+        this.situacao.reprovar(this);
+    }
+    public void finalizar(){
+        this.situacao.finalizar(this);
+    }
+    public int getQunatidadeItens() {
+        return qunatidadeItens;
+    }
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
+    }
+    public String getSituacao() {
+        return situacao;
+    }
+}
