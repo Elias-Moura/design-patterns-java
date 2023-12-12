a33e1d9ebf9ab6afa41cbb13fdda47a7b9d58cb5
Elias Moura <elias.smourayt@gmail.com>
2023-12-12 13:46:05 -0300

    Classe para representar a exception lançada no estado com uma operação inválida.

---
+++ src/alura/orcamento/situacao/exception/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package alura.orcamento.situacao.exception;
+
+public class InvalidOperationException extends RuntimeException {
+    public InvalidOperationException(String s) {
+        super(s);
+    }
+}
